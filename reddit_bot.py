b6b0bd83831472751dbb3096717e36926f2404f3
kennygloggins <kenneth.t.smith@gmail.com>
2020-08-08 22:05:17 -0400

    -create branch for v2

--- reddit_bot.py
+++ reddit_bot.py
@@ -3,37 +3,48 @@
 # File: reddit_bot_tut.py
 # Project: Reddit_bot
 
+import re
+import regex
+from pymongo import MongoClient
+from config import mongserver, name_list
 import praw
-import sqlite3
 
-
-# Uncomment first time running to create database
-#c.execute('CREATE TABLE replied_to (submission text)')
+# Setup connection to mongodb collection
+server = MongoClient(mongserver)
+db = server.twitter_db
+posts = db.reddit_test
 
 # Grab bot info from praw.ini
 reddit = praw.Reddit('bot1')
-replied_to = sqlite3.connect('replied_to.db')
-c = replied_to.cursor()
+
+def find_name(fname):
+	for key, value in name_list.items():
+		for x in value:
+			if re.search(x, fname):
+				return key
+	return 'F1'
 
 # Grab post from a subreddit and only return a url if it hasn't been returned before
 def forumal_dank(sub, count):
-    danger = reddit.subreddit(sub)
-    new_title = []
-    new_post = []
-    for post in danger.hot(limit=10):
-        if post.ups > count and not post.stickied:
-            # Check in database to see if we've posted this submission before
-            c.execute("SELECT * FROM replied_to WHERE submission=?", (post.id,))
-            if c.fetchone():
-                pass
-            else:
-                # Haven't posted before so store submission ID in database
-                c.execute("INSERT INTO replied_to VALUES (:submission)", {'submission': str(post.id)})
-                replied_to.commit()
-                new_title.append(post.title)
-                new_post.append(post.url)
-    return new_post, new_title
+	danger = reddit.subreddit(sub)
+	new_title = []
+	new_post = []
+	new_name = []
+	for post in danger.hot(limit=10):
+		if post.ups > count and not post.stickied:
+			# Check in database to see if we've posted this submission before
+			if db.reddit_test.find_one({"post_id": post.id}):
+				pass
+			else:
+				post_id = { "post_id": post.id }
+				postdb_id = posts.insert_one(post_id).inserted_id
+				# Haven't posted before so store submission ID in database and
+				# append the title and url onto our lists
+				new_title.append(post.title)
+				new_post.append(post.url)
+	for name in new_title:
+		new_name.append(find_name(name))
+	return new_title, new_post, new_name
 
 
-#with concurrent.futures.ThreadPoolExecutor() as executor:
-    #f1 = executor.submit(schedule.every(10).seconds.do(forumal_dank))
+#[new_name.append(x) if a.find(name.lower()) for a in y else new_name.append('F1') for x,y in name_list.items()]
